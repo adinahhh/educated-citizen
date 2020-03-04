212d3e0907e0cd2a3c61d3c8eb141c47cf885d58
vagrant <vagrant@vagrant.vm>
2020-03-04 22:19:02 +0000

    added two html files; added a file in .gitignore

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+from pprint import pformat
+import os
+import requests
+
+app = Flask(__name__)
+app.secret_key = 'SECRETSECRETSECRET'
+
+API_KEY = os.environ['CIVIC_API_KEY']
+
+@app.route('/')
+def homepage():
+    """ Homepage """
+
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
