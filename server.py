f2d4e8589a3df4fce7c3faa55ac85ea3a77d63b9
vagrant <vagrant@vagrant.vm>
2020-03-06 22:52:09 +0000

    added jinja to results.html, modified server.py

--- server.py
+++ server.py
@@ -11,14 +11,37 @@ API_KEY = os.environ['CIVIC_API_KEY']
 
 @app.route('/')
 def homepage():
-    """ Homepage """
+    """ Homepage with user form"""
 
     return render_template('homepage.html')
 
-@app.route('/search_results')
-def search_results():
+@app.route('/search')
+def search_address():
+    """User search for ballot with street address """
 
-    return render_template('results.html')
+# below is for electionQuery
+    election_url = 'https://www.googleapis.com/civicinfo/v2/elections'
+    election_payload = {'key' : API_KEY}
+
+    election_response = requests.get(election_url, params=election_payload)
+
+    election_data = election_response.json()
+    # here i need to figure out how to queue my data
+    elections = election_data['elections']
+
+    return render_template('results.html', elections=elections)
+
+# below is for voterInfoQuery
+# match address from homepage.html with address in payload
+    # voter_address = request.args.get('address', '')
+
+# use elections id from election query as a param in voter_payload
+# will the electionid be stored in a session key?
+
+    # voter_url = 'https://www.googleapis.com/civicinfo/v2/voterinfo'
+    # voter_payload = {'key' : API_KEY, 'voter_address': address, 'electionId' : '2000'}
+    # voter_response = requests.get(voter_url, params=voter_payload)
+    # voter_info_json = voter_response.json() 
 
 
 if __name__ == '__main__':
